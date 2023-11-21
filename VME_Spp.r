d05e9ebfe90e78c893c18ab6e59a024908bc5e2d
Marquez <a39495@hi.no>
2023-11-21 10:24:44 +0100

    List of VME species

---
+++ VME_Spp.r
@@ -0,0 +1,14 @@
+SoftSponge<-c("Aplysilla_sp.", "Aplysilla_arctica","Aplysilla_rosea","Aplysilla_rosea","Geodia.Stelleta", "Geodia.Stryphnus", "Geodia_atlantica","Geodia_barretti","Geodia_macandrewii", "Geodia_phlegraei", "Geodia_sp.", "Stryphnus_ponderosus", "Steletta_grubei", "Stelletta_sp.")
+HardSponge<-c("Phakellia.Axinella", "Phakellia_sp.", "Axinellidae", "Axinella_infundibuliformis", "Antho_dichotoma", "Mycale_lingua", "Craniella_cranium","Craniella_sp","Craniella_zetlandica")
+DeepSponge<-c("Caulophacus_arcticus", "Chondrocladia_gigantea","Cladorhiza_gelida", "Cladorhiza_sp.","Cladorhizidae", "Cladorhizidae_bottlebrush", "Cladorhizidae_branched", "Cladorhizidae_bush","Asbestopluma_pennatula","Asconema_setubalense","Caulophacus_arcticus","Hexactinellida_fan_shaped")
+CoralReefs<-c("Lophelia_pertusa", "Madrepora_oculata")
+SoftGorgonians<-c("Radicipes_sp.", "Isidella_lofotensis")
+CupCorals<-c()
+HardGorgonians<-c("Gorgonian","Paragorgia_arborea", "Primnoa_resedaeformis", "Paramuricea_placomus", "Swiftia_sp.")
+Cauliflowers<-c("Nephtheidae", "Gersemia_rubiformis", "Duva_florida", "Drifa_glomerata", "Malacalcyonacea")
+SylasteridCoral<-c("Stylasteridae")
+SeapenDeep<-c("Umbellula_encrinus")
+SeapenShallow<-c("Funiculina_quadrangularis","Kophobelemnon_stelliferum")
+
+
+ALL_vme<-c(Cauliflowers, HardGorgonians, SoftGorgonians, CoralReefs, DeepSponge, HardSponge, SoftSponge, SylasteridCoral, SeapenDeep, SeapenShallow)
